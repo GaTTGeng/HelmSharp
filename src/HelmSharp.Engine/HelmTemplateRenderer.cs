ca621e36b176b10dca1ef2f85cb82e79f7000031
GaTTGeng <genggatt@gmail.com>
2026-06-20 14:38:50 +0800

    fix(engine): remove resource.k8s.io/v1alpha2 from default API versions
    This API group is not consistently available across Helm 3.12 builds on
    different platforms. Replace the golden test assertion with scheduling.k8s.io/v1
    which is stable and universally present.

--- src/HelmSharp.Engine/HelmTemplateRenderer.cs
+++ src/HelmSharp.Engine/HelmTemplateRenderer.cs
@@ -1306,7 +1306,6 @@ public sealed class HelmTemplateRenderer
         "rbac.authorization.k8s.io/v1",
         "rbac.authorization.k8s.io/v1beta1",
         "rbac.authorization.k8s.io/v1alpha1",
-        "resource.k8s.io/v1alpha2",
         "scheduling.k8s.io/v1alpha1",
         "scheduling.k8s.io/v1beta1",
         "scheduling.k8s.io/v1",
